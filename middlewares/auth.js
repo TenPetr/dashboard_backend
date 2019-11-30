b8bc3e37772a831650a5f9b5885630be44ecde60
Petr Jelínek <peetr.jelinek@gmail.com>
2019-11-30 20:19:24 +0100

    Debug

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const config = require("config");
 
 module.exports = function(req, res, next) {
-  console.log(req.session);
+  console.log(req.signedCookies);
   const token = req.session.jwt;
 
   if (!token) return res.status(401).send("Access denied. No token provided.");
